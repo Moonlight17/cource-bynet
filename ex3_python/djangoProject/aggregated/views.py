5e3456283582e4e197edd0e6e559196806363534
Serov Sergey <Moonlight234@yandex.ru>
2022-09-02 12:42:07 +0300

    Creating json with all information in files

--- ex3_python/djangoProject/aggregated/views.py
+++ ex3_python/djangoProject/aggregated/views.py
@@ -1,57 +1,70 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-import os, fnmatch, csv, pandas, difflib, json
+import fnmatch
+import json
+import os
+import pandas
 from datetime import datetime
 
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
+
 # VARIABLES
-path=""
-data=None
+path = ""
+data = None
 pattern = "participant-*.csv"
-column_in_csv=['Meeting Name', 'Meeting Start Time', 'Meeting End Time', 'Name', 'Attendee Email', 'Join Time', 'Leave Time', 'Attendance Duration', 'Connection Type']
+column_in_csv = ['Meeting Name', 'Meeting Start Time', 'Meeting End Time', 'Name', 'Attendee Email', 'Join Time',
+                 'Leave Time', 'Attendance Duration', 'Connection Type']
+
 
 # function for create list with csv's file
 def finding_all_csv():
     list_files = []
-    fileOfDirectory = os.listdir('.')
+    fileOfDirectory = os.listdir(os.path.join(settings.BASE_DIR, 'csv_files'))
     for filename in fileOfDirectory:
         if fnmatch.fnmatch(filename, pattern):
             list_files.append(filename)
     return list_files
+
 
 def select_date(date_time_str):
     date_time_str = date_time_str[2:-1]
     date_time_obj = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
     return date_time_obj.date()
 
-def read_csv(list_csv):
-    destiny = pandas.DataFrame()
-    for file in list_csv:
-        # data = [{}]
-        try:
-            # print(file)
-            df = pandas.read_csv(file,
-                                 encoding="utf-16",
-                                 sep="\t",
-                                 header=0,
-                                 engine='python')
-            # data = df[['Name']]
-            meeting_date = select_date(df.loc[0]['Meeting Start Time'])
-            df[['Attendance Duration']].replace(r' mins', '')
-            df[['Attendance Duration']] = df[['Attendance Duration']].replace(regex=[r' mins$'], value='').astype('int64')
-            result = df.groupby(by='Attendee Email', dropna=False, sort=False).sum()
 
-            print(result['Attendance Duration'])
-            # return result.to_json()
-            destiny[date] = result['Attendee Email'].map(result)
-
-
-        except OSError:
-            print("kalsdknaoskdn")
+def read_csv(file):
+    meeting_date = datetime
+    result = []
+    try:
+        df = pandas.read_csv(os.path.join(settings.BASE_DIR, 'csv_files', file),
+                             encoding="utf-16",
+                             sep="\t",
+                             header=0,
+                             engine='python')
+        # data = df[['Name']]
+        # print("DFFFFF", df)
+        meeting_date = select_date(df.loc[0]['Meeting Start Time'])
+        df[["Attendance Duration"]].replace(r' mins', '')
+        df[[str(meeting_date)]] = df[["Attendance Duration"]].replace(regex=[r' mins$'], value='').astype(
+            'int64')
+        result = df.groupby(by="Attendee Email", dropna=False, sort=False).sum()
+        print(type(result.to_json()))
+        result=json.loads(result.to_json())
+    except OSError:
+        print("kalsdknaoskdn")
 
         # print(data)
-    return destiny, meeting_date
+    return result, meeting_date
 
+
+def parsing(request):
+    list_csv = finding_all_csv()
+    data = {}
+    all_data = {}
+    for file in list_csv:
+        data, date = read_csv(file)
+        all_data[list(data.keys())[0]] = data[list(data.keys())[0]]
+    print(all_data)
+    return JsonResponse(all_data)
 
 
 def index(request):
