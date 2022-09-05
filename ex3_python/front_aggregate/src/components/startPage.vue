6999bc7d3ef313554e6c50ad40a79e45ee2f6d6a
Serov Sergey <Moonlight234@yandex.ru>
2022-09-05 17:56:17 +0300

    Init front-Project (Aggregate)

---
+++ ex3_python/front_aggregate/src/components/startPage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <button
+        class="btn btn-primary"
+        data-bs-target="#collapseTarget"
+        data-bs-toggle="collapse">
+      Bootstrap collapse
+    </button>
+    <div class="collapse py-2" id="collapseTarget">
+      This is the toggle-able content!
+    </div>
+    <div id="counter">
+      Счётчик: {{ counter }}
+    </div>
+    <button @click="getList()">OLOLO</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'startPage',
+  props: {
+    msg: String
+  },
+    data() {
+      return {
+        counter: 0,
+
+      }
+    },
+  mounted() {
+    // setInterval(() => {
+    //   this.counter++
+    // }, 1000)
+  },
+  methods: {
+    getList() {
+      this.axios.get('http://localhost:8000/aggregate/2022-06-01/2022-10-01/').then((response) => {
+        console.log(response.data)
+      })
+    }
+  }
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
