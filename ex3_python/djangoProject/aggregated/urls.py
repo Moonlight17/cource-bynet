b8e39297660f96064f5cb54db786cd50ed3128ae
Serov Sergey <Moonlight234@yandex.ru>
2022-09-05 12:44:41 +0300

    Added url for get aggregate in json-format

--- ex3_python/djangoProject/aggregated/urls.py
+++ ex3_python/djangoProject/aggregated/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('files/', views.parsingFile, name='parsingFile'),
+    re_path(r'^aggregate/(?P<from_date>\d{4}-\d{2}-\d{2})/(?P<to_date>\d{4}-\d{2}-\d{2})/$', views.my_date_view, name='my_date')
 ]
