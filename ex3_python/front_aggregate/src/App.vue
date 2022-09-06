874b6605eddafed0497e7f34bc5a2afea6f8693e
Serov Sergey <Moonlight234@yandex.ru>
2022-09-06 09:02:18 +0300

    Front: added few page (with table, with start request, etc.)

--- ex3_python/front_aggregate/src/App.vue
+++ ex3_python/front_aggregate/src/App.vue
@@ -1,15 +1,28 @@
 <template>
+<!--  <navigBlock :aggre="aggregates" />-->
   <img alt="Vue logo" src="./assets/logo.png">
+  <h1>TEST</h1>
+  <aggregateTable :aggre="aggregates"/>
   <startPage msg="Welcome to Your Vue.js App"/>
+  <pre>{{$data}}</pre>
 </template>
 
 <script>
-import startPage from './components/startPage.vue'
+import startPage from '@/components/startPage.vue'
+import aggregateTable from '@/components/aggregateTable.vue'
+// import navigBlock from "@/components/navigbar";
 
 export default {
   name: 'App',
   components: {
-    startPage
+    startPage,
+    aggregateTable,
+    // navigBlock
+  },
+  data(){
+    return {
+      aggregates: [],
+    }
   }
 }
 </script>
