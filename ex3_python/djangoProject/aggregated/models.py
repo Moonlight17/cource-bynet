c4f95a30f768d465337d75efd6d7aba4a270e135
Serov Sergei <serov.sergei1790@gmail.com>
2022-09-09 12:33:24 +0300

    Back: Edited format using status

--- ex3_python/djangoProject/aggregated/models.py
+++ ex3_python/djangoProject/aggregated/models.py
@@ -5,9 +5,16 @@ from django.utils.html import format_html
 
 
 class Participants(models.Model):
+    class Status(models.IntegerChoices):
+        Student = 0
+        Employer = 1
+
+
     Name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', default=datetime.datetime.now(), blank=True)
-    status = models.IntegerField("Status Participants (0-student; 1-employer)", default=0)
+    status = models.IntegerField(choices=Status.choices, default=0)
+
+    # status = models.IntegerField("Status Participants (0-student; 1-employer)", default=0)
 
     def __str__(self):
         return self.Name
