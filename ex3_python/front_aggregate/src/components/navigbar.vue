81ee0a23efbc7fa89ec1a5372f77e1da3ca91d97
Serov Sergei <serov.sergei1790@gmail.com>
2022-09-16 13:46:27 +0300

    Front: added classes for dark mode

--- ex3_python/front_aggregate/src/components/navigbar.vue
+++ ex3_python/front_aggregate/src/components/navigbar.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="navigbar">
-    <nav class="navbar navbar-expand-md bg-light">
+    <nav class="navbar navbar-expand-md" :class="[sett ? 'navbar-light bg-light' : 'navbar-dark bg-dark']">
+<!--    <nav class="navbar navbar-expand-md">-->
       <div class="container-fluid">
-        <a class="navbar-brand" id="logotype" href="#"><img alt="Logo" src="../assets/Logotype.png"></a>
+        <a class="navbar-brand" id="logotype" href="#">
+          Serov
+<!--          <img alt="Logo" src="../assets/Logotype.png">-->
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -11,12 +15,13 @@
             <li class="nav-item">
 
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
+<!--            <li class="nav-item">-->
+<!--              <a class="nav-link disabled">Disabled</a>-->
+<!--            </li>-->
           </ul>
           <form class="d-flex" role="search">
-            <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Filters</button>
+            <button type="button" class="btn" :class="[sett ? 'btn-outline-dark' : 'btn-outline-light']" data-bs-toggle="modal" data-bs-target="#exampleModal">Settings</button>
+<!--            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">Settings</button>-->
           </form>
         </div>
       </div>
@@ -24,21 +29,22 @@
     </nav>
     <div>
     </div>
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal fade" :class="[sett ? 'color-dark' : 'color-light']"  id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl">
-          <div class="modal-content">
+          <div class="modal-content" :class="[sett ? 'bg-light' : 'bg-dark']">
             <div class="modal-header">
               <h5 class="modal-title" id="exampleModalLabel">Filters</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <button type="button" :class="[sett ? '' : 'btn-close-white']" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
               <MultiSelect
                   v-model="example.value"
                   v-bind="example"
                   :options="users"
-                  class="multiselect-blue"
+                  class=""
+                  :class="[sett ? 'multiselect-light' : 'multiselect-dark']"
               >
-              </Multiselect>
+              </MultiSelect>
               <br>
               <br>
               <br>
@@ -55,6 +61,7 @@
                   :minDate="minDate"
                   modelType="dd-MM-yyyy"
                   :enableTimePicker="false"
+                  :dark="!sett"
               />
 
             </div>
@@ -78,6 +85,7 @@ export default {
   },
   props: {
     users: Array,
+    sett: Boolean,
   },
   data(){
     return{
@@ -134,9 +142,57 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+.color-light{
+  color:  var(--bs-light);
+}
+.color-dark{
+  color:  var(--bs-dark);
+}
+/*.multiselect-dark {*/
+/*  --ms-bg: #454545;*/
+/*  --ms-tag-bg: #D1FAE5;*/
+/*  --ms-tag-color: #059669;*/
+/*  --ms-border-color: #00ff00;*/
+/*  --ms-placeholder-color: pink;*/
+/*  !*background-color: red;*!*/
+/*}*/
+/*div.multiselect-tags > div > input.multiselect-tags-search{*/
+/*  background-color: var(--ms-bg);*/
+/*}*/
+/*.multiselect input{*/
+/*  background-color: gold;*/
+/*}*/
+.multiselect{
+  --ms-placeholder-color: white;
+  --ms-tag-bg: #0d6efd;
+  --ms-bg: #353535 !important;
+  --ms-border-color: #727272;
+  --ms-dropdown-bg: #353535;
+  --ms-option-color-pointed: white !important;
+  --ms-option-color-selected-pointed: #FFFFFF;
+
+  --ms-option-bg-pointed: #FFFFFF;
+  --ms-option-color-pointed: black !important;
+  --ms-option-color-selected: #FFFFFF;
+  --ms-group-label-bg-selected-pointed: #0d6efd;
+}
+
+input.multiselect-tags-search{
+  background-color: var(--ms-bg) !important;
+}
+.dp__theme_dark {
+  --dp-background-color: #353535 !important;
+  --dp-primary-color: #0d6efd !important;
+  --dp-border-color: gold;
+}
 #logotype{
   width: 10%;
+  font-family: 'Rubik Dirt', cursive;
+  font-size: 2.3rem;
+  color: gold;
+  padding: 0;
+  margin: 0;
 }
 #logotype img{
   width: 100%;
@@ -144,32 +200,24 @@ export default {
 .nav{
   padding: 10px 3%;
 }
-.drop-downbox{
-  /*color: red;*/
-}
 .modal-body{
   margin: 0 auto;
 }
-.dp__theme_light{
-  /*background: #0d6efd !important;*/
-  /*--dp-primary-color: red;*/
-}
-.multiselect-blue{
-  --ms-border-color: #D1D5DB;
-  --ms-tag-bg: #DBEAFE;
-  --ms-tag-color: #2563EB;
-  /*--ms-group-label-bg-selected: red;*/
-  --ms-group-label-bg: #E5E7EB;
-  --ms-group-label-color: #374151;
-  --ms-group-label-bg-pointed: #D1D5DB;
-  --ms-group-label-color-pointed: #374151;
-  --ms-group-label-bg-disabled: #F3F4F6;
-  --ms-group-label-color-disabled: #D1D5DB;
-  --ms-group-label-bg-selected: #0d6efd;
-  --ms-group-label-color-selected: #FFFFFF;
-  --ms-group-label-bg-selected-pointed: #0d6efd;
-  --ms-group-label-color-selected-pointed: #FFFFFF;
-  --ms-group-label-bg-selected-disabled: #6aa5ff;
-  --ms-group-label-color-selected-disabled: #6aa5fc;
-}
+/*.multiselect-blue{*/
+/*  --ms-border-color: #D1D5DB;*/
+/*  --ms-tag-bg: #DBEAFE;*/
+/*  --ms-tag-color: #2563EB;*/
+/*  --ms-group-label-bg: #E5E7EB;*/
+/*  --ms-group-label-color: #374151;*/
+/*  --ms-group-label-bg-pointed: #D1D5DB;*/
+/*  --ms-group-label-color-pointed: #374151;*/
+/*  --ms-group-label-bg-disabled: #F3F4F6;*/
+/*  --ms-group-label-color-disabled: #D1D5DB;*/
+/*  --ms-group-label-bg-selected: #0d6efd;*/
+/*  --ms-group-label-color-selected: #FFFFFF;*/
+/*  --ms-group-label-bg-selected-pointed: #0d6efd;*/
+/*  --ms-group-label-color-selected-pointed: #FFFFFF;*/
+/*  --ms-group-label-bg-selected-disabled: #6aa5ff;*/
+/*  --ms-group-label-color-selected-disabled: #6aa5fc;*/
+/*}*/
 </style>
