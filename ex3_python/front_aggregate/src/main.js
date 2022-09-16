113b7d7721540e99e86fa57a23e5a415066badf8
Serov Sergei <serov.sergei1790@gmail.com>
2022-09-16 13:46:54 +0300

    Front: imported module for context menu

--- ex3_python/front_aggregate/src/main.js
+++ ex3_python/front_aggregate/src/main.js
@@ -13,13 +13,17 @@ import '@vuepic/vue-datepicker/dist/main.css'
 import '@vueform/multiselect/themes/default.css'
 import Multiselect from '@vueform/multiselect'
 
+import VueSimpleContextMenu from 'vue-simple-context-menu';
+import 'vue-simple-context-menu/dist/vue-simple-context-menu.css';
+
+
 const app = createApp(App)
 app.use(VueAxios, axios)
 
 // eslint-disable-next-line
 app.component('Datepicker', Datepicker);
 app.component('MultiSelect', Multiselect);
-
+app.component('VueSimpleContextMenu', VueSimpleContextMenu);
 
 app.mount('#app')
 
