e2dd62e78eec65924dc61c4bde86b41c2e23b1fc
Serov Sergei <serov.sergei1790@gmail.com>
2022-10-10 19:49:38 +0300

    DevOps(Terraform): Added EKS infrastructure

---
+++ FinalProject/Terraform/EKS/main.tf
@@ -0,0 +1,40 @@
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+terraform {
+  backend "s3" {
+    bucket = "bynet-bucket"
+    key    = "tf-states/eks-state.tfstate"
+    region = "eu-west-2"
+    profile = "dev-backup"
+  }
+}
+
+provider "aws" {
+  region = var.region
+  profile = var.profile
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket  = "bynet-bucket"
+    key     = "tf-states/vpc-state.tfstate"
+    region  = "eu-west-2"
+    profile = "dev-backup"
+  }
+}
+
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = data.terraform_remote_state.vpc.outputs.cluster_name
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
