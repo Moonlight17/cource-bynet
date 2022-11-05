eba8e58ad3f36e6f23bc6d2a42a7670d81341c50
Serov Sergei <serov.sergei1790@gmail.com>
2022-11-05 11:44:01 +0200

    DevOps(Terraform): Added helm with autoscaler

---
+++ FinalProject/Terraform/EKS/autoscale-helm.tf
@@ -0,0 +1,44 @@
+
+provider "helm" {
+  kubernetes {
+    config_path = "/Users/moonlight/Documents/Work/Bynet/FinalProject/Kubernetes/eksconfig"
+  }
+}
+resource "helm_release" "cluster-autoscaler" {
+  depends_on = [
+    module.eks
+  ]
+
+  name             = "cluster-autoscaler"
+  namespace        = local.k8s_service_account_namespace
+  repository       = "https://kubernetes.github.io/autoscaler"
+  chart            = "cluster-autoscaler"
+  version          = "9.10.7"
+  create_namespace = false
+
+  set {
+    name  = "awsRegion"
+    value = data.aws_region.current.name
+  }
+  set {
+    name  = "rbac.serviceAccount.name"
+    value = local.k8s_service_account_name
+  }
+  set {
+    name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.iam_assumable_role_admin.iam_role_arn
+    type  = "string"
+  }
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = data.terraform_remote_state.vpc.outputs.cluster_name
+  }
+  set {
+    name  = "autoDiscovery.enabled"
+    value = "true"
+  }
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
+}
